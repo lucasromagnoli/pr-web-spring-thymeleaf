63038ce3c94a48db43cbadda5746a7e6e065e388
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-11-06 21:52:17 -0300

    Inserting and configuring sl4j logger

--- prwstj-web/src/main/java/br/com/lucasromagnoli/prwstj/web/controller/HomeController.java
+++ prwstj-web/src/main/java/br/com/lucasromagnoli/prwstj/web/controller/HomeController.java
@@ -1,5 +1,7 @@
 package br.com.lucasromagnoli.prwstj.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import br.com.lucasromagnoli.prwstj.web.constants.ControllerMapping;
 @RequestMapping(ControllerMapping.PATH_HOME_ROOT)
 public class HomeController {
 
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	@GetMapping
 	public ModelAndView index() {
 		return new ModelAndView(ControllerMapping.VIEW_HOME_INDEX);
